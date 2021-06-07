4abc4d2a0d619021daf14c47a921c648fb0173b5
tina0711 <primo711@naver.com>
2021-06-07 23:32:49 +0900

    Create MyClient.java

---
+++ JETTY/HTTP_CLIENT/MyClient.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.http.HttpMethod;
+
+public class MyClient {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		HttpClient httpClient = new HttpClient();
+		httpClient.start();
+		ContentResponse contentRes = httpClient.newRequest("http://127.0.0.1:8080/mypath").method(HttpMethod.GET).send();
+		System.out.println(contentRes.getContentAsString());
+	}
+
+}
