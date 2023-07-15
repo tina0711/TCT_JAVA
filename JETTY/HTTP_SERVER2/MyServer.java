46c8a588fcb7bcbcef2ce08686a2c9a53cc80204
tina0711 <primo711@naver.com>
2023-07-15 18:29:29 +0900

    Update MyServer.java

--- JETTY/HTTP_SERVER2/MyServer.java
+++ JETTY/HTTP_SERVER2/MyServer.java
@@ -5,8 +5,14 @@ import org.eclipse.jetty.servlet.ServletHandler;
 
 public class MyServer {
 
+	private static Map<String, String> map = new HashMap<String, String>();
+	private static int port = 8080;
+	
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
+
+		map.put("pathPrefix", "/front");
+		
 		new MyServer().start();
 	}
 	
@@ -14,12 +20,17 @@ public class MyServer {
 		Server server = new Server();
 		ServerConnector http = new ServerConnector(server);
 		http.setHost("127.0.0.1");
-		http.setPort(8080);
+		http.setPort(port);
 		server.addConnector(http);
 		
-		ServletHandler servletHandler = new ServletHandler();
-		servletHandler.addServletWithMapping(MyServlet.class, "/*");
-		server.setHandler(servletHandler);
+		//ServletHandler servletHandler = new ServletHandler();
+		//servletHandler.addServletWithMapping(MyServlet.class, "/*");
+
+		ServletContextHandler context = new ServletContextHandler();
+		context.setAttribute("map", map);
+		context.setContextPath("/");
+		context.addServlet(MyServlet.class, "/*");
+		server.setHandler(context);
 		
 		server.start();
 		server.join();
